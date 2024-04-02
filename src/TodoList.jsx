b4809d6864388a6a59de97e59f3a57a14b9e7872
justgogame <justnite@gmail.com>
2024-04-01 22:38:42 -0400

    Lesson 1.4: Lifting State And Props Handling

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,25 +1,14 @@
-import React from 'react';
+import * as React from 'react';
 import TodoListItem from './TodoListItem';
 
-const todoList = [
-  {
-    id: 1,
-    title: "View lecture",
-  },
-  {
-    id: 2,
-    title: "Complete assignment",
-  },
-  {
-    id: 3,
-    title: "Read a book",
-  }
-]
 
-function TodoList(){
+
+function TodoList({todoList}){
     return (
         <ul>
-            {todoList.map((todo) => (<TodoListItem key={todo.id} todo={todo}/>))}
+            {todoList.map((todo) => (
+                <TodoListItem key={todo.id} todo={todo} />
+            ))}
       </ul>
     );
 
