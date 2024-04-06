e99228bd94eef19c35f6db0522e9e9a48cf275ab
justgogame <justnite@gmail.com>
2024-04-05 21:39:53 -0400

    Lesson 1.5: Hooks And Fragments

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,7 +1,9 @@
-import * as React from 'react';
+import { useState } from 'react';
 
-function AddTodoForm({onAddTodo}){
-    const [todoTitle, setTodoTitle] = React.useState('');
+
+function AddTodoForm({onAddTodo}) {
+
+    const [todoTitle, setTodoTitle] = useState('');
 
     const handleTitleChange = (event) => {
         const newTodoTitle = event.target.value;
@@ -11,20 +13,22 @@ function AddTodoForm({onAddTodo}){
     const handleAddTodo = (event) => {
         event.preventDefault();
 
+        console.log(todoTitle);
         onAddTodo({ title: todoTitle, id: Date.now() });
-        setTodoTitle('');
-    }
-    return(
-        <form onSubmit={handleAddTodo}>
-            <label htmlFor='todoTitle'>Title</label>
-            <input 
-                id='todoTitle'
-                type='text' 
-                name='title'
-                value={todoTitle}
-                onChange={handleTitleChange}></input>
-            <button type='submit'>Add</button>
-        </form>
+        setTodoTitle('');    
+    
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleAddTodo}>
+                <label htmlFor="todoTitle">Title: </label>
+                <input id="todoTitle" type='Text' name='title' value={todoTitle} onChange={handleTitleChange}></input>
+                <button type='submit'>Add</button>
+            </form>
+        </div>
     );
+    
 }
+
 export default AddTodoForm;
